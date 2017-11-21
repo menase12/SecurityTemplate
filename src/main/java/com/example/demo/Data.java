58232bb2958244f57b5006992f79b218ceae473b
menase12 <menase.yonatan@gmail.com>
2017-11-21 15:00:43 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Data.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Data implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
